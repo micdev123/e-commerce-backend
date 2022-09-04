c53dcb9e6bfba497af1027d4d64b25b3828aee14
Michael L Bangura <michlawbang123@gmail.com>
2022-09-04 00:24:48 -1200

    done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,11 +5,11 @@ const { registerUser, loginUser } = require('../controllers/authController')
 const router = express.Router();
 
 
-// Login User
-router.post('/sign-in', loginUser)
-
 // Register new user
 router.post('/sign-up', registerUser)
+
+// Login User
+router.post('/sign-in', loginUser)
 
 
 module.exports = router
